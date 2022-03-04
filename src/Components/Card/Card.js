9f5e27f22afeca86c900d5bd1b3593e2949db9e3
James Gardener <jamesgardener92@gmail.com>
2022-03-04 07:28:51 +0000

    Added Navbar and Card component files. Updated gitignore to include env

---
+++ src/Components/Card/Card.js
@@ -0,0 +1,11 @@
+import styles from './Card.module.css'
+
+const Card = () => {
+    return(
+        <div className={styles.wrapper}>
+            <p>Good morning, from Card</p>
+        </div>
+    )
+}
+
+export default Card;
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <div>
+            <p>Good morning, from the Navbar</p>
+        </div>
+    )
+}
+
+export default Navbar

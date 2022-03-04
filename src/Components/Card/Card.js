4ff1e1cc47ed93b029330c4542f32003a122169b
James Gardener <jamesgardener92@gmail.com>
2022-03-04 13:05:47 +0000

    Created Card component

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,11 +1,46 @@
 import styles from './Card.module.css'
+import {useState, useEffect} from 'react';
 
 const Card = () => {
-    return(
-        <div className={styles.wrapper}>
-            <p>Good morning, from Card</p>
-        </div>
-    )
+const [movie, getMovie] = useState(null)
+    useEffect(() => {
+        const getData = () => {
+            fetch(`https://www.omdbapi.com/?t=The+Last+Samurai&apikey=${process.env.REACT_APP_API_KEY}`)
+            .then((response) => response.json())
+            .then((data) => getMovie(data));
+        }
+        getData();
+    }, []);
+
+    console.log(movie);
+
+    if(movie == null) {
+        return (
+            <div>
+                <p>Loading...</p>
+            </div>
+        )
+    } else {
+        return (
+            <div className={styles.wrapper}>
+                <div className={styles.movieImage}>
+                    <img src={movie.Poster} alt="Movie Poster"/>
+                </div>
+                <div className={styles.movieInfo}>
+                    <h1 className={styles.title}>{movie.Title}</h1>
+                    <p className={styles.director}>{movie.Director}</p>
+                    <ul className={styles.infoList}>
+                        <li>Released: <br/>{movie.Released}</li>
+                        <li>Language: <br/>{movie.Language}</li>
+                        <li>Rated: <br/>{movie.Rated}</li>
+                    </ul>
+                    <p className={styles.plot}>{movie.Plot}</p>
+                    <p className={styles.actors}>Starring: {movie.Actors}</p>
+
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Card;
